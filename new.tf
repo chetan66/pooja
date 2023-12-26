3d170e199ffc4f62a2d0a5ca0373c99fd1f55dbf
chetan66 <chetandeshmukh1994@gmail.com>
2023-12-26 18:04:29 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIA27HJ3O63Q7I46J5M"
-    secret_key = "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"
+    access_key = "AK*****************"
+    secret_key = "****************88**************N"
   
 }
